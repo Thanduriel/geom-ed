1f44d6105524d0fdff071959aad4e260b82addbe
Thanduriel <rojendersie@alice.de>
2022-06-26 17:45:18 +0200

    + basic cubic simulation

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include "simulation/cubicmesh.hpp"
+#include <iostream>
+
+constexpr double c = 1.0;
+
+int main()
+{
+	using namespace sim;
+
+	using FloatT = double;
+	using Mesh = CubicMesh<FloatT>;
+	using Index = Mesh::Index;
+
+	constexpr FloatT ds = 1.0;
+	constexpr FloatT dt = 0.57;
+
+	constexpr int64_t n = 32;
+	const Mesh::SizeVec size{ n,n,n };
+	Mesh mesh(size, Mesh::Vec{ ds,ds,ds });
+
+	using Integrator = CubicIntegrator<double>;
+	Integrator integrator(mesh, dt);
+	for (int i = 0; i < 4096; ++i) 
+	{
+		double pTotal = 0.0;
+		double divBTotal = 0.0;
+		for (Index iz = 0; iz < size.z; ++iz)
+			for (Index iy = 0; iy < size.y; ++iy)
+				for (Index ix = 0; ix < size.x; ++ix)
+				{
+					pTotal += std::abs(mesh.p(ix, iy, iz));
+					divBTotal += std::abs(mesh.divB(ix, iy, iz));
+				}
+		std::cout << divBTotal << " | " << pTotal << "\n";
+
+	/*	for (Index iy = 0; iy < size.y; ++iy)
+		{
+			for (Index ix = 0; ix < size.x; ++ix)
+				std::cout << mesh.B(ix, 16, iy).x << " ";
+			std::cout << "\n";
+		}*/
+	//	std::cout << mesh.E(16, 16, 16).x << "\n";
+		integrator.step(mesh);
+	}
+}
---
+++ tests/test.py
@@ -0,0 +1,26 @@
+import fdtd
+import numpy as np
+
+def div(B):
+	return B[1:, :-1, :-1, 0] - B[:-1, :-1, :-1, 0] + B[:-1, 1:, :-1, 1] - B[:-1, :-1, :-1, 1] + B[:-1, :-1, 1:, 2] - B[:-1, :-1, :-1, 2]
+
+grid = fdtd.Grid(
+    (32, 32, 32),
+    1.0,
+    1.0,
+    1.0
+)
+grid[0, :, :] = fdtd.PeriodicBoundary(name="xbounds")
+grid[:, 0, :] = fdtd.PeriodicBoundary(name="ybounds")
+grid[:, :, 0] = fdtd.PeriodicBoundary(name="zbounds")
+#grid[-1, :, :] = fdtd.PeriodicBoundary(name="xbounds1")
+#grid[:, -1, :] = fdtd.PeriodicBoundary(name="ybounds1")
+#grid[:, :, -1] = fdtd.PeriodicBoundary(name="zbounds1")
+
+grid.E[16,16,16,0] = 1.0
+print("time-step: {}".format(grid.time_step))
+print(grid)
+for i in range(0,128):
+	print(np.sum(np.abs(div(grid.H))))
+	grid.run(1, False)
+	#print(grid.E[16,16,16,0])
